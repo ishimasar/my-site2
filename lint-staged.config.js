40b3bcb4b17b8df3ae7c6f4e68e839cc24d7d7f5
masa_ishii <ishimasar1106@gmail.com>
2023-10-31 20:45:29 +0900

    Lintを入れてみた

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 export default {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   'src/**/*.{js,jsx,ts,tsx}': 'eslint --fix --no-ignore --max-warnings=0',
-  'src/**/*.{css,scss}': 'stylelint --fix'
+  'src/**/*.{css,scss}': 'stylelint',
+  'src/**/*.{astro}': 'htmlhint'
 }
