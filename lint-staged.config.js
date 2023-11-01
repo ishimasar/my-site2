da8bdab5a304d9b99b1df0d0503945090f532027
masa_ishii <ishimasar1106@gmail.com>
2023-11-01 14:22:11 +0900

    調整

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ export default {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   'src/**/*.{js,jsx,ts,tsx}': 'eslint --fix --no-ignore --max-warnings=0',
   'src/**/*.{css,scss}': 'stylelint',
-  'src/**/*.{astro}': 'htmlhint'
+  'src/**/*.astro': 'htmlhint'
 }
