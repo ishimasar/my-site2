b81dc4331b863dba0e3e4b741cecff0d66b48cfa
masa_ishii <ishimasar1106@gmail.com>
2023-11-14 15:09:58 +0900

    htmlhint、eslint設定更新

---
+++ eslintrc.config.js
@@ -0,0 +1,50 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import jsdoc from "eslint-plugin-jsdoc";
+import html from 'eslint-plugin-html';
+import markdown from 'eslint-plugin-markdown';
+import js from '@eslint/js';
+import typeScriptESLint from '@typescript-eslint/eslint-plugin';
+import typeScriptESLintParser from '@typescript-eslint/parser';
+
+const compat = new FlatCompat();
+
+export default [
+  {
+    ignores: ['']
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...compat.extends(
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ),
+  {
+    plugins: {
+      jsdoc,
+      typeScriptESLint,
+      html,
+      markdown
+    },
+    languageOptions: {
+      globals: {
+      },
+      parser: typeScriptESLintParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2021
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'no-debugger': 'error',
+      'jsdoc/require-description': 'error',
+      'jsdoc/check-values': 'error',
+      'node/no-deprecated-api': 'off',
+      'node/no-unpublished-import': 'off',
+      'node/no-unpublished-require': 'off',
+      'node/no-unsupported-features/es-syntax': 'off',
+      'no-process-exit': 'off',
+      'node/no-missing-import': 'off'
+    }
+  }
+]
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   // '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  'src/**/*.{js,jsx,ts,tsx}': 'eslint --fix --no-ignore --max-warnings=0',
+  '**/*.{js,jsx,ts,tsx}': 'eslint',
   '**/*.{css,scss}': 'stylelint',
   'src/**/*.astro': 'htmlhint'
 }
