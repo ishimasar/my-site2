473822428e87f00919b48127d15191f3e1abdee3
masa_ishii <ishimasar1106@gmail.com>
2023-11-14 20:31:22 +0900

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '**/*.ts': 'eslint',
+  '**/*.{ts,astro}': 'eslint',
   '**/*.{css,scss}': 'stylelint',
-  '**/blog/*.html **/blog/**/*.html': 'htmlhint'
+  '**/*.astro': 'htmlhint --config .htmlhintrc'
 }
