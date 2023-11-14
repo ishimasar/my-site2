f7ae4da72e6072242ab9d85cd4fb913910184285
masa_ishii <ishimasar1106@gmail.com>
2023-11-14 19:25:54 +0900

    Lint内容アップデート2

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '**/*.{js,jsx,ts,tsx}': 'eslint',
+  '**/*.ts': 'eslint',
   '**/*.{css,scss}': 'stylelint',
-  'src/**/*.astro': 'htmlhint'
+  '**/blog/*.html **/blog/**/*.html': 'htmlhint'
 }
